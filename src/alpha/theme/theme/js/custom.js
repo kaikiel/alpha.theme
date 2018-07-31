c8bdbcfebc75c0df16ed9fe820ea7808b2bb0656
Kiel <kiel@mingtak.com.tw>
2018-07-31 07:09:17 +0000

    update product field

--- src/alpha/theme/theme/js/custom.js
+++ src/alpha/theme/theme/js/custom.js
@@ -1,4 +1,29 @@
 $( document ).ready(function() {
+// map
+$('map area').click(function(){
+    className = $(this).attr('id')
+    $('.map-layer').hide()
+    $('.' + className).show()
+    $('.map-com').hide()
+    $('.' + className + '-com').show()
+})
+
+/*
+$('map area').hover( function(){
+    className = $(this).attr('id')
+//    $('.' + className).show()
+}, function(){
+    className = $(this).attr('id')
+    display = $(this).css('display')
+//debugger
+//    if
+//    $('.' + className).hide()
+
+} )
+*/
+
+
+
     $(".modal").on("show.bs.modal", function(){
         setTimeout(function(){ 
             $('.flexslider').resize();
